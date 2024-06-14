e84019553948e0a40849c1172e5e06facf0b79ec
Caua <acauamedeiros@gmail.com>
2024-06-14 16:24:36 -0300

    avaliacao de logica

---
+++ avaliacao-14/ex01.py
@@ -0,0 +1,18 @@
+<script language="JavaScript" type="text/JavaScript">
+ 
+  <!--
+var saudacao
+
+if {saudacao = "Bom Dia -"}
+if {saudacao = "Boa Tarde -"}
+if {saudacao = "Boa Noite -"}
+ 
+diaSem = new Array(7);
+ 
+diaSem[0] = "Domingo";
+diaSem[1] = "Segunda-feira";
+diaSem[2] = "Terça-feira";
+diaSem[3] = "Quarta-feira";
+diaSem[4] = "Quinta-feira";
+diaSem[5] = "Sexta-feira";
+diaSem[6] = "Sábado";
